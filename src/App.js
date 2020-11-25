a620adb7778e582d7abb8f9fab0de9c13aff8116
Albert Valdes <albert.valdes@24g.com>
2020-11-25 05:39:33 -0600

    Added our work movie details with routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import MovieDetail from './pages/MovieDetail';
+import ContactUs from './pages/ContactUs';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/" component={AboutUs} />
+        <Route exact path="/work" component={OurWork} />
+        <Route exact path="/work/:id" component={MovieDetail} />
+        <Route exact path="/contact" component={ContactUs} />
+      </Switch>
     </div>
   );
 }
